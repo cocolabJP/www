51351f7de9db05306da4c4f66edd172cde02baad
yukimatJP <yukimat.jp@gmail.com>
2025-03-13 12:40:34 +0900

    update top-visual ipsjone

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ const $ = (id) => {
 }
 
 const SLIDE_ITEMS = [
+  {"img": "202503_ipsjone", "url": "https://ipsj-one.org/"},
   {"img": "202503_lab-pv", "url": "https://youtu.be/f9qyBV11SAA"},
   // {"img": "202502_lab-intro", "url": "https://cocolabjp.tumblr.com/post/775807831215685632"},
   // {"img": "202501_newyear", "url": "/"},
