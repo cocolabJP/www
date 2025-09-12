2b5e4fdeec4f1bf622e6dd40e875798de21ff683
yukimatJP <yukimat.jp@gmail.com>
2025-09-12 21:52:55 +0900

    update 20250912

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,8 @@ const $ = (id) => {
 }
 
 const SLIDE_ITEMS = [
-  {"img": "202505_ITS101", "url": "https://cocolabjp.tumblr.com/post/785216546183200768/its101award"},
+  {"img": "202509_ICMU2025", "url": "https://cocolabjp.tumblr.com/post/794487082880040960"},
+  {"img": "202505_ITS101", "url": "https://cocolabjp.tumblr.com/post/785216546183200768"},
   {"img": "202504_kickoff", "url": "https://cocolabjp.tumblr.com/post/780186889493856256"},
   {"img": "202504_move", "url": "https://cocolabjp.tumblr.com/post/779575363018850305"},
   // {"img": "202503_newcomers", "url": "https://cocolabjp.tumblr.com/post/778560442957250560"},
