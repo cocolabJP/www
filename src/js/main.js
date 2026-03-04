f46f723f22fd670a47094b46cd57100d66ba0008
yukimatJP <yukimat.jp@gmail.com>
2026-03-04 11:52:34 +0900

    update 20260304

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ const $ = (id) => {
 }
 
 const SLIDE_ITEMS = [
+  {"img": "202603_newcomers", "url": "https://cocolabjp.tumblr.com/post/810107648615776256"},
   {"img": "202602_inno-vation", "url": "https://cocolabjp.tumblr.com/post/809692145400381440"},
   {"img": "202511_lab-pv", "url": "https://youtu.be/Sd9_H3f3yPA"},
   {"img": "202509_ipsjKansai", "url": "https://cocolabjp.tumblr.com/post/796035693426196480"},
